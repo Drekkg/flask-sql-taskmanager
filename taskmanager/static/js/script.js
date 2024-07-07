74b9a84075d25dfb53437837a4eea0dbc326f7ab
Derek Garnett <rizzagum@gmail.com>
2024-07-07 08:26:53 +0000

    FEAT adds add task functionality and add_task.html

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -5,3 +5,15 @@ document.addEventListener('DOMContentLoaded', function() {
     M.Sidenav.init(sidenav);
   });
 
+// date-picker
+  let datepicker= document.querySelectorAll('.datepicker');
+  M.Datepicker.init(datepicker, {
+    format: "dd mmmm, yyyy",
+    i18n: {done : "Select"}
+  });
+
+
+
+//select init
+let selects = document.querySelectorAll('select');
+M.FormSelect.init(selects);
